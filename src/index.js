af9c393d14d7cf621f94a382135e9be0baca9017
jbelagamba <jaquelinebdemoraes@gmail.com>
2020-07-28 22:02:31 -0300

    [scrapbook] cria rota para editar scrap

--- src/index.js
+++ src/index.js
@@ -19,12 +19,35 @@ app.get("/scraps", (request, response) => {
   return response.json(results);
 });
 
+// criar scrap
 app.post("/scraps", (request, response) => {
   const { title, message } = request.body;
 
   const scrap = { id: uuid(), title, message };
 
   scraps.push(scrap);
+
+  return response.json(scrap);
+});
+
+// editar scrap
+app.put("/scraps/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, message } = request.body;
+
+  const scrapIndex = scraps.findIndex((scrap) => scrap.id == id);
+
+  if (scrapIndex < 0) {
+    return response.status(400).json({ error: "scrap not found." });
+  }
+
+  const scrap = {
+    id,
+    title,
+    message,
+  };
+
+  scraps[scrapIndex] = scrap;
 
   return response.json(scrap);
 });
